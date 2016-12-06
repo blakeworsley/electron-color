7adb821872ea8a4be001a16b992e98736d3c6621
kylem038 <misenckj@gmail.com>
2016-12-06 13:28:05 -0700

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    minWidth: 800,
+    minHeight: 600
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+});
