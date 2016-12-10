15ef2617abd0cec81f882c86e88cc4208c428920
Blake Worsley <blakeworsley@Blakes-MacBook-Pro.local>
2016-12-09 22:15:22 -0700

    adds eyedropper with esc and e key shortcut

--- app/script.js
+++ app/script.js
@@ -8,6 +8,9 @@ const $hexValue = $('.hex-value');
 const $hexContainer = $('.hex-container');
 const $rgbaContainer = $('.rgba-containter');
 
+const $eyedropperView = $('.eyedropper-view');
+
+const $eyedropperButton = $('.eyedropper-button');
 const $hexValueButton = $('.hex-value-button');
 
 const $redValueInput = $('.red-value-input');
@@ -76,8 +79,8 @@ function updateColor(){
   let alpha = $alphaValueInput.val();
   let rgba = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
   let hex = rgbToHex(rgba);
-  updateBackgroundColor(red, green, blue);
   updateGradientColor(red, green, blue, hex);
+  updateBackgroundColor(red, green, blue);
   $hexValue.html(hex);
 }
 
@@ -90,17 +93,46 @@ function updateGradientColor(red, green, blue, hex) {
   $gradientColor.css({'background-image': gradient});
 }
 
-function getDropperColor() {
-  const position = robot.getMousePos();
+function updateEyedropperView() {
+  const { position, dropperColor } = getDropperColor();
+  $eyedropperView.css({ 
+    'top': `${position.y-70}px`, 
+    'left': `${position.x-40}px`,
+    'border': `solid 20px #${dropperColor}`, 
+    'opacity': '1'
+  });
+}
+
+function getMousePosition(){
+  return robot.getMousePos();
+}
+
+function getDropperColor(){
+  const position = getMousePosition();
   const dropperColor = robot.getPixelColor(position.x, position.y);
-  console.log(dropperColor);
+  const color = {position, dropperColor};
+  return color;
 }
 
 function hexToRgb(hex) {
   hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
              ,(m, r, g, b) => '#' + r + r + g + g + b + b)
     .substring(1).match(/.{2}/g)
-    .map(x => parseInt(x, 16))
+    .map(x => parseInt(x, 16));
 }
 
-$('html').on('click', () => { getDropperColor() });
+$eyedropperButton.on('click', () => {
+  $eyedropperView.toggle();
+  updateEyedropperView()
+  $('html').on('mousemove', () => { 
+    updateEyedropperView();
+  });
+  $('html').on('click', () => {
+    console.log(getDropperColor());
+  });
+});
+
+$(document).keyup(function(e) {
+  if (e.keyCode == 69) {$eyedropperView.toggle()};
+  if (e.keyCode == 27) {$bodyBackground.toggle()};
+});
