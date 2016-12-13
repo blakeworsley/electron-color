c97085b804ff8321897ccded626570292c1bff6b
Blake Worsley <blakeworsley@gmail.com>
2016-12-13 10:39:21 -0700

    adds closeable menu

--- app/main.js
+++ app/main.js
@@ -1,8 +1,7 @@
-const { app, Menu, Tray, BrowserWindow } = require('electron');
+const { app, Menu, BrowserWindow } = require('electron');
 const storage = require('electron-storage');
 
 let mainWindow = null;
-let tray = null;
 
 app.on('ready', () => {
   doesStorageExist();
@@ -14,17 +13,7 @@ app.on('ready', () => {
     transparent: true,
   });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
-  console.log(Tray);
-  
-  tray = new Tray(`./app/img/eyedropper.png`);
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'},
-    {label: 'Item3', type: 'radio', checked: true},
-  ])
-  tray.setToolTip('Electron Color');
-  tray.setContextMenu(contextMenu);
-  // Menu.setApplicationMenu(menu);
+  Menu.setApplicationMenu(menu);
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
   });
@@ -53,8 +42,7 @@ const retrieveDataFromStorage = exports.retrieveDataFromStorage = () => {
   storage.get('saved-colors')
     .then(data => {
       mainWindow.webContents.send('retrieved-colors', data);
-    }).then((response) => console.log(response) )
-    .catch(err => {
+    }).catch(err => {
       console.log(err);
     });
 };
@@ -86,134 +74,28 @@ const saveCurrentColor = exports.saveCurrentColor = (color) => {
 //data model//
 const defaultData = { "current": {"r": 0, "g": 0, "b": 0, "a": 1 }, "saved": [] };
 
-// const template = [
-//   {
-//     label: 'View',
-//     submenu: [
-//       {
-//         role: 'reload'
-//       },
-//       {
-//         role: 'toggledevtools'
-//       },
-//       {
-//         type: 'separator'
-//       },
-//       {
-//         role: 'resetzoom'
-//       },
-//       {
-//         role: 'zoomin'
-//       },
-//       {
-//         role: 'zoomout'
-//       },
-//       {
-//         type: 'separator'
-//       },
-//       {
-//         role: 'togglefullscreen'
-//       }
-//     ]
-//   },
-//   {
-//     role: 'window',
-//     submenu: [
-//       {
-//         role: 'minimize'
-//       },
-//       {
-//         role: 'close'
-//       }
-//     ]
-//   },
-//   {
-//     role: 'help',
-//     submenu: [
-//       {
-//         label: 'Learn More',
-//         click () { require('electron').shell.openExternal('http://electron.atom.io') }
-//       }
-//     ]
-//   }
-// ]
+const template = [
+  {
+    role: 'window',
+    submenu: [
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      },
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        role: 'quit'
+      },
+    ]
+  }
+];
 
-// if (process.platform === 'darwin') {
-//   template.unshift({
-//     label: app.getName(),
-//     submenu: [
-//       {
-//         role: 'about'
-//       },
-//       {
-//         type: 'separator'
-//       },
-//       {
-//         role: 'services',
-//         submenu: []
-//       },
-//       {
-//         type: 'separator'
-//       },
-//       {
-//         role: 'hide'
-//       },
-//       {
-//         role: 'hideothers'
-//       },
-//       {
-//         role: 'unhide'
-//       },
-//       {
-//         type: 'separator'
-//       },
-//       {
-//         role: 'quit'
-//       }
-//     ]
-//   })
-//   // Edit menu.
-//   template[1].submenu.push(
-//     {
-//       type: 'separator'
-//     },
-//     {
-//       label: 'Speech',
-//       submenu: [
-//         {
-//           role: 'startspeaking'
-//         },
-//         {
-//           role: 'stopspeaking'
-//         }
-//       ]
-//     }
-//   )
-//   // Window menu.
-//   template[3].submenu = [
-//     {
-//       label: 'Close',
-//       accelerator: 'CmdOrCtrl+W',
-//       role: 'close'
-//     },
-//     {
-//       label: 'Minimize',
-//       accelerator: 'CmdOrCtrl+M',
-//       role: 'minimize'
-//     },
-//     {
-//       label: 'Zoom',
-//       role: 'zoom'
-//     },
-//     {
-//       type: 'separator'
-//     },
-//     {
-//       label: 'Bring All to Front',
-//       role: 'front'
-//     }
-//   ]
-// }
-
-// const menu = Menu.buildFromTemplate(template);
-// console.log(menu);
+const menu = Menu.buildFromTemplate(template);
+console.log(menu);
